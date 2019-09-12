8a053684f7925d4d6748e7565d88cb84bc0e5839
JoseMPerez <josepr121110@gmail.com>
2019-09-11 23:30:43 -0700

    Working on navigation bar

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,14 @@ export default class Login extends Component {
   render() {
     return (
       <div>
-        <h1>Login</h1>
+        
+		
+		<ul class="nav-bar">
+		  <li class="b-left"><a href="#home">Spartan Sell</a></li>
+		  <li class="b-right"><a href="#clients">Signup</a></li>  
+		  <li class="b-right"><a href="#contact">Login</a></li>
+		</ul>	
+		
       </div>
     );
   }
