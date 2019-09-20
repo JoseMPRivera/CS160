09de084ee6ecb18e453761e9c93c5cc3d5c8450b
JoseMPerez <josepr121110@gmail.com>
2019-09-19 23:44:00 -0700

    Working of navbars

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 export default class HomePage extends Component {
   render() {
     return (
+
       <div>
-        <h1>Home</h1>
-        <a href="/Login">Login</a>
-      </div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Spartan Sells</a>
+  <img src="/images /log.png" alt=""></img>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>  
+
+
+      <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">Spartan Sells</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="/Login">Login</Nav.Link>
+      <NavDropdown title="User" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Rating</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Profile</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Selling</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Configuration</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+
+
+</div>
     );
   }
 }
