d78e1808e21457dc1206404f217371e06dac6845
Mohammad Syed <mohammad@mohammads-mbp-2.attlocal.net>
2019-09-27 21:15:22 -0700

    adding input text boxes for login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
+import { Button, FormGroup, FormControl } from "react-bootstrap";
 import '../css/Login.css';
+import { faTextWidth } from '@fortawesome/free-solid-svg-icons';
 
 export default class Login extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+    email: "",
+    password: ""
+  };
+}
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+  trackChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
   render() {
     return (
       <div>
@@ -12,7 +34,36 @@ export default class Login extends Component {
         </ul>	
 
         <div class="vl"></div>
+        <div className = "Login">
+          <form onSubmit={this.handleSubmit}>
+            <FormGroup controlId="email" bsSize="small">
+              <FormControl
+                autoFocus
+                type="email"
+                value={this.state.email}
+                onChange={this.trackChange}
+                />
+            </FormGroup>
 
+            <FormGroup controlId="password" bsSize="medium">
+              <FormControl
+                autoFocus
+                type="password"
+                value={this.state.password}
+                onChange={this.trackChange}
+                />
+            </FormGroup>
+
+            <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+          </form>
+        </div>
 
       </div>
     );
