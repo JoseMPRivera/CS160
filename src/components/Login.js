01485ead17c70ef8f13b0388f8df1b2129df965a
JoseMPerez <josepr121110@gmail.com>
2019-09-30 11:12:00 -0700

    home

--- src/components/Login.js
+++ src/components/Login.js
@@ -29,7 +29,7 @@ export default class Login extends Component {
       <form>
       <div>
         <ul class="nav-bar">
-          <li class="b-left"><a href="#home">Spartan Sell</a></li>
+          <li class="b-left"><a href="/">Spartan Sell</a></li>
           <li class="b-right"><a href="#clients">Signup</a></li>  
           <li class="b-right"><a href="#contact">Login</a></li>
         </ul>	
