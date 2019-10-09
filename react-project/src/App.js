ba36b2a0b9fe37ef53c947831d695191e993d428
Mohammad Syed <mohammad@Mohammads-MacBook-Pro-2.local>
2019-10-09 11:37:45 -0700

    Connected the ReactJs frontend with the NodeJS backend

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {Component} from 'react';
+import logo from './logo.svg';
+
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/HomePage'
 import Login from './components/Login'
@@ -6,15 +8,53 @@ import signUp from './components/signUp'
 import Profile from './components/Profile'
 import './App.css';
 
-function App() {
+
+
+class App extends Component {
+  state = {
+      data: null
+    };
+
+componentDidMount() {
+  // Call our fetch function below once the component mounts
+this.callBackendAPI()
+  .then(res => this.setState({ data: res.express }))
+  .catch(err => console.log(err));
+}
+// Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+callBackendAPI = async () => {
+const response = await fetch('/express_backend');
+const body = await response.json();
+
+if (response.status !== 200) {
+  throw Error(body.message) 
+}
+return body;
+};
+// function App() {
+//   return (
+//     <Router>
+//       <Route path="/" exact component={Home} />
+//       <Route path="/Login" component={Login} />
+//       <Route path="/signUp" component={signUp} />
+//       <Route path="/Profile" component={Profile} />
+//     </Router>
+//   );
+// }
+
+
+render() {
   return (
     <Router>
       <Route path="/" exact component={Home} />
       <Route path="/Login" component={Login} />
       <Route path="/signUp" component={signUp} />
       <Route path="/Profile" component={Profile} />
+      <p className="App-intro">{this.state.data}</p>
     </Router>
+    
   );
+}
 }
 
 export default App;
