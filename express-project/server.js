06e7fb67c445a5e90b5ea9ec86644776cc92e729
Mohammad Syed <mohammad@Mohammads-MacBook-Pro-2.local>
2019-10-09 12:06:15 -0700

    Connecting MySQL server tto NodeJS

--- express-project/server.js
+++ express-project/server.js
@@ -9,3 +9,16 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
+});
+
+con.connect(function(err) {
+  if (err); //throw err;
+  console.log("Connected!");
+});
