1906124963d3d76faeffe7643c59b9c6fbb002fc
Mohammad Syed <mohammad@Mohammads-MacBook-Pro-2.local>
2019-10-10 16:34:22 -0700

    Updating our database

--- express-project/server.js
+++ express-project/server.js
@@ -15,10 +15,14 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: ""
+  password: "",
+  database: "cs160"
 });
 
 con.connect(function(err) {
-  if (err); //throw err;
-  console.log("Connected!");
+  if (err) throw err;
+  con.query("SELECT * FROM customers", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
 });
