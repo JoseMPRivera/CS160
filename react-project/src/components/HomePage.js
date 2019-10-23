1c27e0dcfe22ac59a9cfb7b909800b16b8fa43cd
twei0407 <47849840+twei0407@users.noreply.github.com>
2019-10-23 11:34:42 -0700

    adding homebar

--- react-project/src/components/HomePage.js
+++ react-project/src/components/HomePage.js
@@ -1,42 +1,13 @@
 import React, { Component } from 'react';
 import '../css/Login.css';
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Carousel } from 'react-bootstrap';
+import HomeBar from './HomeBar';
 
 export default class HomePage extends Component {
   render() {
     return (
       <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          {/* <a class="navbar-brand" href="#">Spartan Sells</a> */}
-          {/* <img src="https://i.ibb.co/VBw9f9S/Spartan-Sells-Logo-2x.png" width="60" height="60" alt="spartan-Sells"/> */}
-          <img src= {require ("./image/SpartanSells.png")} width="60" height="60" alt="spartan-Sells" />
-          {/*<img src="https://image.flaticon.com/icons/png/512/21/21294.png" class={""/*"img-responsive"*/}
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
-              </li>
-            </ul>
-          </div>
-          <Nav className="mr-auto">
-              <NavDropdown title="User" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Rating</NavDropdown.Item>
-                {/* <NavDropdown.Item href="#action/3.2">Profile</NavDropdown.Item> */}
-                <NavDropdown.Item href="/Profile">Profile</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Selling</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Configuration</NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="/Login">Login</Nav.Link>
-            </Nav>
-        </nav>  
-
+        <HomeBar />
 
         <Navbar bg="light" expand="lg">
           <Navbar.Brand href="#home">Spartan Sells</Navbar.Brand>
