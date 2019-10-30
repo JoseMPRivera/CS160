0f2487282c2f0dccd820211ce3bad7af130f890b
Mohammad Syed <mohammad@Mohammads-MacBook-Pro-2.local>
2019-10-29 17:06:41 -0700

    Adding landing page for signup and rating, creating a landing ref for signup on login component

--- express-project/server.js
+++ express-project/server.js
@@ -15,7 +15,7 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "sh100",
+  password: "",
   database: "cs160"
 });
 
--- react-project/src/components/Rating.js
+++ react-project/src/components/Rating.js
@@ -2,64 +2,67 @@ import React, { Component } from 'react';
 //import '../css/Rating.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser, faFlag } from '@fortawesome/free-solid-svg-icons'
-
+import HomeBar from './HomeBar'
 export default class Rating extends Component {
-
-  render() {
-    return (
-        <div class="container">
-          <div class="row profile">
-            <div class="col-md-3">
-              <div class="profile-sidebar">
-
-                <div class="profile-userpic">
-                  <img src="https://image.flaticon.com/icons/png/512/21/21294.png" class={""/*"img-responsive"*/} alt="" />
-                </div>
-
-                <div class="profile-usertitle">
-                  <div class="profile-usertitle-name">
-                    John Smith
+    render() {
+      return (
+        <div>
+          <HomeBar />
+          <div class="container">
+            <div class="row profile">
+              <div class="col-md-3">
+                <div class="profile-sidebar">
+  
+                  <div class="profile-userpic">
+                    <img src="https://image.flaticon.com/icons/png/512/21/21294.png" class={""/*"img-responsive"*/} alt="" />
                   </div>
-                  <div class="profile-usertitle-job">
-                    First Year
+  
+                  <div class="profile-usertitle">
+                    <div class="profile-usertitle-name">
+                      Rating
+                    </div>
+                    <div class="profile-usertitle-job">
+                      First Year
+                    </div>
                   </div>
+  
+                  <div class="profile-userbuttons">
+                    <button type="button" class="btn btn-danger btn-sm">Message</button>
+                  </div>
+  
+                  <div class="profile-usermenu">
+                    <ul class="profile-nav">
+                      <li class="active">
+                        <a href="#">
+                        <FontAwesomeIcon icon={faHome}/>
+                        Overview </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                        <FontAwesomeIcon icon={faUser}/>
+                        Account Settings </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                        <FontAwesomeIcon icon={faFlag}/>
+                        Help </a>
+                      </li>
+                    </ul>
+                  </div>
+  
                 </div>
-
-                <div class="profile-userbuttons">
-                  <button type="button" class="btn btn-danger btn-sm">Message</button>
-                </div>
-
-                <div class="profile-usermenu">
-                  <ul class="profile-nav">
-                    <li class="active">
-                      <a href="#">
-                      <FontAwesomeIcon icon={faHome}/>
-                      Overview </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                      <FontAwesomeIcon icon={faUser}/>
-                      Account Settings </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                      <FontAwesomeIcon icon={faFlag}/>
-                      Help </a>
-                    </li>
-                  </ul>
-                </div>
-
               </div>
-            </div>
-
-            <div class="col-md-9">
-              <div class="profile-content">
-                Some user related content goes here
+  
+              <div class="col-md-9">
+                <div class="profile-content">
+                  Some user related content goes here
+                </div>
               </div>
+  
             </div>
-
           </div>
         </div>
-    );
+      );
+    }
   }
-}
+  
